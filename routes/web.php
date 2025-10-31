f3db76e66c5c669dde41b075b24666b2a9111156
Moises-Silva-Dev <moises.gonzalez.dev@gmail.com>
2025-10-31 11:27:00 -0600

    Files Created for CRUD Membership

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,9 @@ Route::middleware('auth')->group(function () {
 
     // Gym
     Route::resource('gym', controller: App\Http\Controllers\GymController::class);
+
+    // Memberships
+    Route::resource('memberships', controller: App\Http\Controllers\MembershipController::class);
 });
 
 require __DIR__ . '/auth.php';
